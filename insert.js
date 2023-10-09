3774cff19bc371498edb1a17f0aff5d7a035a2e3
ashmitchhoker <143075877+ashmitchhoker@users.noreply.github.com>
2023-10-09 18:18:18 +0530

    Add files via upload

---
+++ insert.js
@@ -0,0 +1,74 @@
+const http = require('http');
+const fs = require('fs'); // Node.js file system module
+const server = http.createServer();
+const WebSocket = require('ws');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '@123pass',
+  database: 'cricket',
+});
+
+const wss = new WebSocket.Server({ server });
+
+// Serve the HTML file when a client accesses the root URL
+server.on('request', (req, res) => {
+  if (req.url === '/') {
+    // Read the HTML file and serve it
+    fs.readFile('insert_index.html', 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading insert_index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  }
+});
+
+// Initialize the WebSocket server
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    // Handle incoming messages (e.g., insert data into the database)
+    const data = JSON.parse(message);
+
+    if (data.action === 'insertData') {
+      // Insert data into the database
+      const { MatchID, TeamID, RunsScored, WicketsTaken, OversBowled } = data;
+      const insertQuery = `INSERT INTO live_score (MatchID, TeamID, RunsScored, WicketsTaken, OversBowled) VALUES (?, ?, ?, ?, ?)`;
+      db.query(
+        insertQuery,
+        [MatchID, TeamID, RunsScored, WicketsTaken, OversBowled],
+        (err, result) => {
+          if (err) {
+            console.error('Error inserting data:', err);
+          } else {
+            console.log('Data inserted successfully');
+            // Notify clients about the update
+            wss.clients.forEach((client) => {
+              if (client !== ws && client.readyState === WebSocket.OPEN) {
+                // Send a message to the client
+                client.send('Data has been updated!');
+              }
+            });
+          }
+        }
+      );
+    }
+  });
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+server.listen(8080, () => {
+  console.log('WebSocket server is running on port 8080');
+});
---
+++ update.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+
+// Create a MySQL database connection
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '@123pass',
+  database: 'cricket',
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile('C:\\Users\\Ashmi\\IBP\\update_index.html'); // Provide the full path to your index.html file
+});
+
+// Define an API endpoint to fetch the most recent live score
+app.get('/api/live_score/latest', (req, res) => {
+  // Query the database to fetch the most recent live score
+  db.query('SELECT * FROM live_score ORDER BY LiveScoreID DESC LIMIT 1', (err, results) => {
+    if (err) {
+      console.error('Error fetching latest live score:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(results[0]); // Send the first (and only) result as JSON
+  });
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
